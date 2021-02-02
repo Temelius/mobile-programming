e1d4af81b69ede2b32eb9ecae465f0dffdeb9a95
Temelius <teme989@gmail.com>
2021-02-02 09:34:31 +0200

    Add task 2 - Number guessing game

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native'
 
 export default function App() {
+
+  const [guess, setGuess] = useState()
+  const [number, setNumber] = useState(() => {
+    return Math.floor(Math.random() * 100) + 1
+  })
+  const [result, setResult] = useState('Guess a number between 1-100')
+  const [tries, setTries] = useState(0)
+
+  const resetGame = () => {
+    setGuess()
+    setNumber(Math.floor(Math.random() * 100) + 1)
+    setResult('Guess a number between 1-100')
+    setTries(0)
+  }
+
+  const startGame = () => {
+    if (guess == number) {
+      Alert.alert('You guessed the number in ' + tries + ' guesses')
+    } else if (guess > number) {
+      setResult('Your guess ' + guess + ' is too high')
+      setTries(tries + 1)
+    } else if (guess < number) {
+      setResult('Your guess ' + guess + ' is too low')
+      setTries(tries + 1)
+    }
+  }
+
+  const Separator = () => (
+    <View style={styles.separator} />
+  )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{result}</Text>
+      <TextInput style={styles.input}
+        keyboardType={'numeric'}
+        onChangeText={text => setGuess(text)}
+        value={guess}
+        textAlign={'center'}
+      />
+      <Button title='MAKE GUESS' onPress={startGame} />
+      <Separator />
+      <Button title='RESET' onPress={resetGame} />
     </View>
   );
 }
@@ -18,4 +57,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: 50,
+    borderColor: 'gray',
+    borderWidth: 2,
+    marginVertical: 10
+  },
+  separator: {
+    marginVertical: 10
+  }
 });
