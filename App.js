164bdf522894d11507f37940cbaac98e494fc921
Temelius <teme989@gmail.com>
2021-02-02 08:35:31 +0200

    Add task 1 - calculator

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 
 export default function App() {
+
+  const [result, setResult] = useState(0)
+  const [numberOne, setNumberOne] = useState(0)
+  const [numberTwo, setNumberTwo] = useState(0)
+
+  const sum = () => {
+    setResult(parseInt(numberOne) + parseInt(numberTwo))
+  }
+
+  const subtract = () => {
+    setResult(parseInt(numberOne) - parseInt(numberTwo))
+  }
+
+  const Spacer = () => (
+    <View style={styles.spacer} />
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Result: {result}</Text>
+      </View>
+      <View>
+        <TextInput style={styles.inputField}
+          keyboardType={'numeric'}
+          textAlign={'center'}
+          onChangeText={text => setNumberOne(text)}
+          value={numberOne.toString()}
+        />
+      </View>
+      <View>
+        <TextInput style={styles.inputField}
+          keyboardType={'numeric'}
+          textAlign={'center'}
+          onChangeText={text => setNumberTwo(text)}
+          value={numberTwo.toString()}
+        />
+      </View>
+
+      <View style={styles.buttons}>
+        <Button title='+' onPress={sum} />
+        <Spacer />
+        <Button title='-' onPress={subtract} />
+      </View>
     </View>
   );
 }
@@ -18,4 +58,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputField: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 2,
+    margin: 5
+  },
+  buttons: {
+    flexDirection: 'row'
+  },
+  spacer: {
+    marginHorizontal: 15
+  }
 });
