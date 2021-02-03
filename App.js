24032c7e0520d9f7447218c54966c38b0966db8b
Temelius <teme989@gmail.com>
2021-02-03 11:27:27 +0200

    Add task 4 - Shopping List

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, FlatList, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [shoppingList, setShoppingList] = useState([])
+  const [item, setItem] = useState()
+
+  const addToShoppingList = () => {
+    setShoppingList([...shoppingList, item])
+  }
+
+  const clearShoppingList = () => {
+    setShoppingList([])
+  }
+
+  const Separator = () => (
+    <View style={styles.separator}></View>
+  )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.inputField}
+        textAlign={'center'}
+        onChangeText={text => setItem(text)}
+        value={item}
+      />
+      <View style={styles.buttons}>
+        <Button title='ADD' onPress={addToShoppingList}/>
+        <Separator />
+        <Button title='CLEAR' onPress={clearShoppingList}/>
+      </View>
+      <Text style={styles.title}>Shopping List</Text>
+      <FlatList
+        data={shoppingList}
+        renderItem={({item}) => <Text>{item}</Text>}
+      />
     </View>
   );
 }
@@ -18,4 +47,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  separator: {
+    marginHorizontal: 10
+  },
+  inputField: {
+    marginTop: 200,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 2,
+    margin: 5
+  },
+  buttons: {
+    flexDirection: 'row'
+  },
+  title: {
+    marginVertical: 20,
+    color: 'blue',
+    fontSize: 20
+  }
 });
