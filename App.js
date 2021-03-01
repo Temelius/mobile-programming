242a2f60be88f5f6e6949029ed8088eb1885bd87
Temelius <teme989@gmail.com>
2021-03-01 08:19:02 +0200

    Add task 8 - Find the address

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Alert, TextInput, Button } from 'react-native';
+import MapView, { Marker } from 'react-native-maps'
 
 export default function App() {
+  const [location, setLocation] = useState('')
+  const [region, setRegion] = useState({
+    latitude: 60.200692,
+    longitude: 24.934302,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221
+  })
+
+  const [marker, setMarker] = useState({
+    latitude: 60.201373,
+    longitude: 24.934041
+  })
+
+  const searchLocation = () => {
+    const key = 'svxdrSVCU87mW45d9yrO2epeA7gV7xBP'
+
+    fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=${key}&inFormat=json&outFormat=json&location=${location}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const result = {
+          ...region, latitude: data.results[0].locations[0].latLng.lat,
+          longitude: data.results[0].locations[0].latLng.lng
+        }
+        setRegion(result)
+        setMarker(result)
+      })
+      .catch((error) => Alert.alert('Error', error.message))
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} region={region}>
+        <Marker coordinate={marker} />
+      </MapView>
+      <View style={styles.searchBar}>
+      <TextInput
+        style={styles.input}
+        textAlign={'center'}
+        value={location}
+        onChangeText={(value) => setLocation(value)}
+      />
+      <Button title='Search' onPress={searchLocation}/>
+      </View>
     </View>
   );
 }
@@ -18,4 +57,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    flex:1,
+    width: '100%',
+
+  },
+  input: {
+    borderBottomWidth: 1,
+    marginBottom: 10
+  },
+  searchBar: {
+    width: '100%'
+  }
 });
