492e98ce7997d84e7390541963a27434bf27b1ed
Temelius <teme989@gmail.com>
2021-03-15 07:21:07 +0200

    Add task 12 - text to speech

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import * as Speech from 'expo-speech'
 
 export default function App() {
+  const [text, setText] = React.useState('')
+
+  const speak = () => {
+    Speech.speak(text)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        onChangeText={(value) => setText(value)}
+        />
+      <Button title='Speak' onPress={speak} />
+
     </View>
   );
 }
@@ -18,4 +29,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: '50%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 15
+  }
 });
